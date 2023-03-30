5c56ddc9add2aac86133c19164d29673224fb953
roudra323 <asirroudra@gmail.com>
2023-03-30 23:27:27 +0600

    completed 5 (use hooks)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/1-useState/final/5-useState-counter.js";
+import Setup from "./tutorial/1-useState/setup/5-useState-counter.js";
 function App() {
   return (
     <div className="container">
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
-};
+  let [count, setCount] = useState(0);
 
+  const incClick = () => {
+    console.log(count);
+    count += 1;
+    setCount(count);
+  };
+
+  const decClick = () => {
+    console.log(count);
+    count -= 1;
+    setCount(count);
+  };
+
+  const resClick = () => {
+    console.log(count);
+    count = 0;
+    setCount(count);
+  };
+
+  return (
+    <React.Fragment>
+      <h2>{count}</h2>
+      <button type="button" className="btn" onClick={() => incClick()}>
+        increase
+      </button>
+      <button type="button" className="btn" onClick={() => decClick()}>
+        decrease
+      </button>
+      <button type="button" className="btn" onClick={() => resClick()}>
+        reset
+      </button>
+    </React.Fragment>
+  );
+};
 export default UseStateCounter;
