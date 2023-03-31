72a839d03784ad557e2d668e71f9b40df449e222
roudra323 <asirroudra@gmail.com>
2023-03-31 22:04:26 +0600

    reduced code:

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -3,34 +3,16 @@ import React, { useState } from "react";
 const UseStateCounter = () => {
   let [count, setCount] = useState(0);
 
-  const incClick = () => {
-    console.log(count);
-    count += 1;
-    setCount(count);
-  };
-
-  const decClick = () => {
-    console.log(count);
-    count -= 1;
-    setCount(count);
-  };
-
-  const resClick = () => {
-    console.log(count);
-    count = 0;
-    setCount(count);
-  };
-
   return (
     <React.Fragment>
       <h2>{count}</h2>
-      <button type="button" className="btn" onClick={() => incClick()}>
+      <button type="button" className="btn" onClick={() => setCount(count + 1)}>
         increase
       </button>
-      <button type="button" className="btn" onClick={() => decClick()}>
+      <button type="button" className="btn" onClick={() => setCount(count - 1)}>
         decrease
       </button>
-      <button type="button" className="btn" onClick={() => resClick()}>
+      <button type="button" className="btn" onClick={() => setCount(0)}>
         reset
       </button>
     </React.Fragment>
